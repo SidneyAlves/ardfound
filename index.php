6fce008ce03096115c93ec0c38a852345ebbfce0
Sidney Alves <sidney.27.alves@gmail.com>
2019-01-23 14:32:03 -0200

    init

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Ard Foundation</title>
+
+    <!-- Bootstrap -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <section class="container-fluid" id="menu-topo">
+        <div class="container">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-2">
+                    <img src="images/logo.png" alt="" width="100%">
+                </div>
+
+                <div class="col-md-10" id="menu-links">
+                    <ul>
+                        <li class="menu-item align-self-center"><a href="#">home</a></li>
+                        <li class="menu-item"><a href="#">ardfoundation</a></li>
+                        <li class="menu-item"><a href="#">founder</a></li>
+                        <li class="menu-item"><a href="#">ambassadors</a></li>
+                        <li class="menu-item"><a href="#">board</a></li>
+                        <li class="menu-item"><a href="#">articles</a></li>
+                        <li class="menu-item"><a href="#">cases</a></li>
+                        <li class="menu-item"><a href="#">events</a></li>
+                        <li class="menu-item"><a href="#">clipping</a></li>
+                        <li class="menu-item"><a href="#">privacy</a></li>
+                        <li class="menu-item"><a href="#">policy</a></li>
+                        <li class="menu-item"><a href="#">contact</a></li>
+                        <a href="#"><img src="images/icon-fb.png" alt="" width="24px"></a>
+                        <a href="#"><img src="images/icon-insta.png" alt="" width="24px"></a>
+                        <a href="#"><img src="images/icon-linkedin.png" alt="" width="24px"></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="bg-yellow">
+        <div class="row">
+            <div class="container">
+                <div class="col-sm-4 offset-sm-4">
+                    <input class="form-control" type="text" placeholder="search for people, trends, pages and groups" aria-label="Search">
+                </div> 
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="carousel">
+        <div class="row">
+                
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="images/slider-1.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="images/slider-1.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="images/slider-1.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </div>
+
+        </div>
+    </section>
+
+    <section class="container-fluid" id="blog">
+        <div class="row">
+            <div class="container">
+                <h2 class="blog-title"><strong>Ard</strong> blog <img src="images/icon-blog.png" alt=""width="40px"></h2>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 instagram">Instagram</div>
+                <div class="col-md-6 instagram">Instagram</div>
+            </div>
+        </div>
+    
+    <div class="text-center" id="verMaisBlog"><a href="#"><strong>veja mais</strong></a></div>
+
+    </section>
+
+    <section class="container-fluid" id="about">
+        <img src="images/topo-about.png" alt="" width="100%">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 offset-md-2">
+                    <img src="images/about.png" alt="" width="100%" id="img-about">
+                </div>
+
+                <div class="col-md-4 offset-md-1">
+                    <hr style="float:left">
+                    <br><br>
+                    <h2>Our Mission</h2>
+                    <p>The ARD Foundation mission is to raise funds and foster basic and translational research 
+                        to identify genetic causes of specific cancers, understand how genes contribute to the disease,
+                        and use that understanding to help develop better prevention and treatment strategies.</p>
+
+                    <h2>Our Vision</h2>
+                    <p>The ARD Foundation’s President, Ms. Ana Paola Diniz, created the foundation having in mind the millions of patients 
+                        that suffer from cancer all over the world.
+                        Preventing and treating cancer still poses a major challenge and affects not only patients but also friends and family.</p>    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="help">
+        <div class="container">
+                <h2>How Can You Help?</h2>
+                <hr>
+                <div class="col-md-4 offset-md-4">
+                    <p>Ms. Ana Paola Diniz believes that together, we can help these
+                        researchers achieve the common goal for all of us:</p>
+                </div>
+                <div class="row">
+                    <div class="col-md-2 offset-md-2" style="padding-top:20px"><img src="images/help1.png" alt="" width="100px" height="100px"></div>
+                    <div class="col-md-2" style="padding-top:20px"><img src="images/help2.png" alt="" width="100px" height="100px"></div>
+                    <div class="col-md-2" style="padding-top:20px"><img src="images/help3.png" alt="" width="100px" height="110px"></div>
+                    <div class="col-md-2" style="padding-top:20px"><img src="images/help4.png" alt="" width="100px" height="100px"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="helped">
+        <div class="container">
+            <h2>Who Us Helped</h2>
+            <hr>
+            <div class="col-md-4 offset-md-4">
+                <p>Ms. Ana Paola Diniz believes that together, we can help these researchers achieve the common goal for all of us:</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4" style="padding-top:20px"><img src="images/helped1.jpg" alt="" width="160px" height="100px"></div>
+                <div class="col-md-4" style="padding-top:20px"><img src="images/helped2.png" alt="" width="200px" height="80px"></div>
+                <div class="col-md-4" style="padding-top:20px"><img src="images/helped3.png" alt="" width="200px" height="80px"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="partners">
+        <div class="container">
+            <h2><strong>ard</strong> partners <img src="images/icon-partners.png" alt="" width="40px"></h2>
+            <div class="row">
+                <div class="col-md-3" style="padding-top:20px"><img src="images/partner1.png" alt="" width="200px" height="120px"></div>
+                <div class="col-md-3" style="padding-top:20px"><img src="images/partner2.png" alt="" width="200px" height="100px"></div>
+                <div class="col-md-3" style="padding-top:20px"><img src="images/partner3.jpg" alt="" width="200px" height="80px"></div>
+                <div class="col-md-3" style="padding-top:20px"><img src="images/partner4.png" alt="" width="200px" height="100px"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="follow">
+        <div class="container">
+            <h2>@ardfoundation</h2>
+            <h2>follow us</h2>
+            <hr>
+            <div id="pluginFollow">
+
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="contact">
+        <div class="row">
+            <div class="col-md-6 padding30" id="contact-img"><img src="images/contact.jpg" alt="" width="100%"></div>
+
+            <div class="col-md-6 padding30" id="contact-form">
+                <div class="col-md-9">
+                    <h2>get in contact with us</h2>
+                    <p>Send us suggestions of themes, speakers, sponsors and other reasons that can bring reflections to our work:</p>
+
+                    <form action="#" method="post">
+                        <input type="text" name="name" id="name" placeholder="name*" required witdh="100%">
+                        <input type="text" name="email" id="email" placeholder="email*" required witdh="100%">
+                        <input type="text" name="subject" id="subject" placeholder="subject" witdh="100%">
+                        <input type="text" name="phone" id="phone" placeholder="phone*" witdh="100%">
+                        <textarea name="message" id="message" placeholder="message*" witdh="100%"></textarea>
+                        <div class="col-md-4 offset-md-4">
+                            <input type="submit" value="Send" name="submit" id="submit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="container-fluid" id="footer">
+        <div class="container-fluid" id="donate">
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4">
+                        <span id="donate-Text">Help Cancer Research</span>
+                        <span>enter an amount to donate</span>
+                    </div>
+
+                    <div class="col-md-4">
+                        <form action="#" method="post">
+                            <input type="text" name="donate" id="donate" placeholder="$">
+                            <input type="submit" name="submit" id="submit" value="PAY">
+                        </form>
+                    </div>
+
+                    <div class="col-md-4">
+                        <p>together we are stronger</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="container" id="footer-bottom">
+
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="images/logo.png" alt="" width="100%">
+                </div>
+
+                <div class="col-md-4">
+                    <h6>USA | ardimpressa@ardfoundation.com</h6>
+                    <h6>BRA | ardimpressa@ardfoundation.com.br</h6>
+                    <h6>
+                        <img src="images/icon-mail.png" alt="" width="24px"> contact@ardfound.org 
+                        <a href="#"><img src="images/icon-fb.png" alt="" width="24px"></a>
+                        <a href="#"><img src="images/icon-insta.png" alt="" width="24px"></a>
+                        <a href="#"><img src="images/icon-linkedin.png" alt="" width="24px"></a>
+                    </h6>
+                </div>
+
+                <div class="col-md-4">
+                    <img src="images/charitylogo.png" alt="" height="70px">
+                    <img src="images/guidestarlogo.png" alt="" height="70px">
+                </div>
+            </div>
+            
+        </div>        
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
