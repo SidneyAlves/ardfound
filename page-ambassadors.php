55c049c580e86fbe790e058460b8b4a757a4d254
sid <sid>
2019-01-29 01:13:59 -0200

    ambassadors quase pronta

--- page-ambassadors.php
+++ page-ambassadors.php
@@ -1,5 +1,116 @@
 <?php include('components/header.php'); ?>
 
+<div class="container-fluid" style="padding: 0px; padding-bottom:50px">
+	<img src="<?php echo get_bloginfo('template_url') ?>/images/banner-ambassadors.jpg" alt="" width="100%">
+</div>
 
+<div class="container-fluid">
+	<div class="container ambassadors-content">
+		<div class="row">
+			<div class="col-md-3">
+				<img src="<?php echo get_bloginfo('template_url') ?>/images/ambassadors/will-smith.jpg" alt="" width="140%">
+			</div>
+			<div class="col-md-5 ambassadors-board">
+				<div class="col-md-9 offset-md-2">
+					<h2>“Graduated from the University of São Paulo, where he received” </h2>
+					<p style="font-size: 11px">Founder of the Cozinha Fit, Silvia Pozzobon shares her healthy and delicious recipes on her
+					website. She graduated from UFRJ and now works as a nutritionist, helping adults and elderly
+					live healthier lives. A cancer survivor herself, Pozzobon teamed up with ARD Foundation to
+					create the StepUpToHealth Program, promoting cancer research and giving lectures on the
+					importance of maintaining healthy habits. She also promotes our work through her social media
+					pages.</p>
+					<hr>
+					<h2>Silvia Pozzobon</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid" style="background-color:#f9f9f9">
+	<div class="container ambassadors-content" >
+		<div class="row">
+			<div class="col-md-5 ambassadors-board">
+				<div class="col-md-9 offset-md-2">
+					<h2>“Graduated from the University of São Paulo, where he received” </h2>
+					<p style="font-size: 11px">A famous Brazilian model and actor, Gianecchine starred in numerous of Brazil’s most famous
+					telenovelas and films. He graduated with a law degree from Pontifical Catholic University of São
+					Paulo and worked with fashion houses in Paris, London, New York, Berlin, and Milan. Since he
+					was diagnosed with cancer in August 2011, he’s been working tirelessly to raise awareness for
+					cancer treatment.</p>
+					<hr>
+					<h2>Reynaldo Gianecchini</h2>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<img src="<?php echo get_bloginfo('template_url') ?>/images/ambassadors/michelle-obama.jpg" alt="" width="140%" style="float:left">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="container ambassadors-content">
+		<div class="row">
+			<div class="col-md-3">
+				<img src="<?php echo get_bloginfo('template_url') ?>/images/ambassadors/will-smith.jpg" alt="" width="140%">
+			</div>
+			<div class="col-md-5 ambassadors-board">
+				<div class="col-md-9 offset-md-2">
+					<h2>“Graduated from the University of São Paulo, where he received” </h2>
+					<p style="font-size: 11px">Having worked as a nurse prior to her modeling career, Ribeiro knows very well the challenges
+					patients and medical staff face on a daily basis. Since 2009, she has worked with a numerous of
+					world renowned designers, including Versace, Chanel, Gucci, Shiatzy Chen, Louis Vuitton, and
+					more. She’s also done numerous appearances for the Victoria Secret’s Fashion Show. As an
+					ambassador, Ribeiro promotes the ARD Foundation through her social media pages.</p>
+					<hr>
+					<h2>Lais Ribeiro</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid" style="background-color:#f9f9f9">
+	<div class="container ambassadors-content" >
+		<div class="row">
+			<div class="col-md-5 ambassadors-board">
+				<div class="col-md-9 offset-md-2">
+					<h2>“Graduated from the University of São Paulo, where he received” </h2>
+					<p style="font-size: 11px">A former model and fashion editor, and current television host and singer, Parlatore started her
+					career while working in Japan. In 2015 she was diagnosed with breast cancer, and after
+					undergoing treatment, she became a dedicated and passionate spokesperson who hopes to raise
+					awareness for early detection. She was the recipient of the ARD Foundation’s Voice of Hope
+					Award, given out during our 2018 Gala in Houston, TX.</p>
+					<hr>
+					<h2>Sabrina Parlatore</h2>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<img src="<?php echo get_bloginfo('template_url') ?>/images/ambassadors/michelle-obama.jpg" alt="" width="140%" style="float:left">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid" style="margin-bottom:50px">
+	<div class="container ambassadors-content">
+		<div class="row">
+			<div class="col-md-3">
+				<img src="<?php echo get_bloginfo('template_url') ?>/images/ambassadors/will-smith.jpg" alt="" width="140%">
+			</div>
+			<div class="col-md-5 ambassadors-board">
+				<div class="col-md-9 offset-md-2">
+					<h2>“Graduated from the University of São Paulo, where he received” </h2>
+					<p style="font-size: 11px">Actress, model, and singer, Emilia Pedersen has achieved a lot in her 14 years of life. With a
+					Danish father, a Brazilian mother, and living in New York, Pedersen is fluent in four different
+					languages, has appeared in magazines, and starred in a numerous plays.</p>
+					<hr>
+					<h2>Emilia Pedersen</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php include('components/footer.php'); ?>
